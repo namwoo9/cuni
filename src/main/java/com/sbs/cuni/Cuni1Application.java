9f91afc6ddfbf10c86fe3e9f1e441d716eed9690
namwoo9 <nwgu99@gmail.com>
2019-12-31 11:58:25 +0900

    cuni1application 재생성

--- src/main/java/com/sbs/cuni/Cuni1Application.java
+++ src/main/java/com/sbs/cuni/Cuni1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CuniApplication {
+public class Cuni1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CuniApplication.class, args);
+		SpringApplication.run(Cuni1Application.class, args);
 	}
 
 }
